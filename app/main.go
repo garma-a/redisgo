7ca026a9848239eb5a31ddc4024ed6494992eef5
Girgis <girgisemad74@gmail.com>
2026-04-04 03:00:53 +0200

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -2,30 +2,45 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"os"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
-var _ = net.Listen
-var _ = os.Exit
-
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
+	// 1. Bind to the port
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
-
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
+	defer l.Close()
 
-	conn, err2 := l.Accept()
-	if err2 != nil {
+	// 2. Accept a single connection
+	conn, err := l.Accept()
+	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-	conn.Write([]byte("+PONG\r\n"))
+	defer conn.Close()
 
+	// 3. Loop to handle multiple commands on the same connection
+	buf := make([]byte, 1024)
+	for {
+		// Read data from the connection
+		n, err := conn.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				// Client closed the connection
+				break
+			}
+			fmt.Println("Error reading from connection: ", err.Error())
+			break
+		}
+
+		// Since the task says we can hardcode PONG for now:
+		if n > 0 {
+			conn.Write([]byte("+PONG\r\n"))
+		}
+	}
 }
