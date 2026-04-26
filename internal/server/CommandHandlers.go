e4e67afb492840a5cff55f52968a3d6a23093f56
Girgis <girgisemad74@gmail.com>
2026-04-26 04:02:05 +0300

    codecrafters submit [skip ci]

--- internal/server/CommandHandlers.go
+++ internal/server/CommandHandlers.go
@@ -10,8 +10,12 @@ import (
 	"github.com/GARMA-A/redisgo/internal/store"
 )
 
-func handleGet(conn net.Conn, db *store.DB, parts []string) {
-	val, exists := db.GetWithTTL(parts[1])
+func handleEcho(conn net.Conn, args []string) {
+	conn.Write([]byte(fmt.Sprintf("$%d\r\n%s\r\n", len(args[0]), args[0])))
+}
+
+func handleGet(conn net.Conn, db *store.DB, args []string) {
+	val, exists := db.GetWithTTL(args[0])
 	if exists {
 		conn.Write([]byte(fmt.Sprintf("$%d\r\n%s\r\n", len(val), val)))
 	} else {
@@ -19,64 +23,66 @@ func handleGet(conn net.Conn, db *store.DB, parts []string) {
 	}
 }
 
-func handleRPush(conn net.Conn, db *store.DB, parts []string) {
+func handleRPush(conn net.Conn, db *store.DB, args []string) {
 	var length int
-	if len(parts) > 3 {
-		length = db.RPushMany(parts[1], parts[2:])
+	if len(args) > 2 {
+		length = db.RPushMany(args[0], args[1:])
 	} else {
-		length = db.RPush(parts[1], parts[2])
+		length = db.RPush(args[0], args[1])
 	}
 	conn.Write([]byte(fmt.Sprintf(":%d\r\n", length)))
 }
 
-func handleLRange(conn net.Conn, db *store.DB, parts []string) {
-	start, _ := strconv.ParseInt(parts[2], 10, 64)
-	stop, _ := strconv.ParseInt(parts[3], 10, 64)
-	list := db.LRange(parts[1], int(start), int(stop))
+func handleLRange(conn net.Conn, db *store.DB, args []string) {
+	start, _ := strconv.ParseInt(args[1], 10, 64)
+	stop, _ := strconv.ParseInt(args[2], 10, 64)
+	list := db.LRange(args[0], int(start), int(stop))
 	conn.Write([]byte(fmt.Sprintf("*%d\r\n", len(list))))
 	for _, item := range list {
 		conn.Write([]byte(fmt.Sprintf("$%d\r\n%s\r\n", len(item), item)))
 	}
 }
-func handleLPush(conn net.Conn, db *store.DB, parts []string) {
+
+func handleLPush(conn net.Conn, db *store.DB, args []string) {
 	var length int
-	if len(parts) > 3 {
-		length = db.LPushMany(parts[1], parts[2:])
+	if len(args) > 2 {
+		length = db.LPushMany(args[0], args[1:])
 	} else {
-		length = db.LPush(parts[1], parts[2])
+		length = db.LPush(args[0], args[1])
 	}
 	conn.Write([]byte(fmt.Sprintf(":%d\r\n", length)))
 }
 
-func handleLLEN(conn net.Conn, db *store.DB, parts []string) {
-	conn.Write([]byte(fmt.Sprintf(":%d\r\n", db.LLEN(parts[1]))))
+func handleLLEN(conn net.Conn, db *store.DB, args []string) {
+	conn.Write([]byte(fmt.Sprintf(":%d\r\n", db.LLEN(args[0]))))
 }
 
-func handleLPop(conn net.Conn, db *store.DB, parts []string) {
-	if len(parts) > 2 {
-		handleLPopMany(conn, db, parts)
-	} else {
-		val := db.LPop(parts[1])
-		if val != "" {
-			conn.Write([]byte(fmt.Sprintf("$%d\r\n%s\r\n", len(val), val)))
-		} else {
-			conn.Write([]byte("$-1\r\n"))
-		}
+func handleLPop(conn net.Conn, db *store.DB, args []string) {
+	if len(args) > 1 {
+		handleLPopMany(conn, db, args)
+		return
 	}
 
+	val := db.LPop(args[0])
+	if val != "" {
+		conn.Write([]byte(fmt.Sprintf("$%d\r\n%s\r\n", len(val), val)))
+	} else {
+		conn.Write([]byte("$-1\r\n"))
+	}
 }
-func handleLPopMany(conn net.Conn, db *store.DB, parts []string) {
-	count, _ := strconv.Atoi(parts[2])
-	values := db.LPopMany(parts[1], count)
+
+func handleLPopMany(conn net.Conn, db *store.DB, args []string) {
+	count, _ := strconv.Atoi(args[1])
+	values := db.LPopMany(args[0], count)
 	conn.Write([]byte(fmt.Sprintf("*%d\r\n", len(values))))
 	for _, val := range values {
 		conn.Write([]byte(fmt.Sprintf("$%d\r\n%s\r\n", len(val), val)))
 	}
 }
 
-func handleBLPOP(conn net.Conn, db *store.DB, parts []string) {
-	key := parts[1]
-	timeoutSec, err := strconv.ParseFloat(parts[2], 64)
+func handleBLPOP(conn net.Conn, db *store.DB, args []string) {
+	key := args[0]
+	timeoutSec, err := strconv.ParseFloat(args[1], 64)
 	if err != nil {
 		conn.Write([]byte("-ERR invalid timeout\r\n"))
 		return
@@ -97,26 +103,24 @@ func handleBLPOP(conn net.Conn, db *store.DB, parts []string) {
 			conn.Write([]byte("*-1\r\n"))
 		}
 	} else {
-		// timeout 0 means block forever
 		val := <-ch
 		conn.Write([]byte(fmt.Sprintf("*2\r\n$%d\r\n%s\r\n$%d\r\n%s\r\n", len(key), key, len(val), val)))
 	}
 }
 
-func handleType(conn net.Conn, db *store.DB, parts []string) {
-	key := parts[1]
-	myType := db.GetType(key)
+func handleType(conn net.Conn, db *store.DB, args []string) {
+	myType := db.GetType(args[0])
 	conn.Write([]byte(fmt.Sprintf("+%s\r\n", myType)))
 }
 
-func handleSet(conn net.Conn, db *store.DB, parts []string) {
-	key := parts[1]
-	val := parts[2]
+func handleSet(conn net.Conn, db *store.DB, args []string) {
+	key := args[0]
+	val := args[1]
 	var expiry time.Time
 
-	if len(parts) >= 5 {
-		option := strings.ToUpper(parts[3])
-		expiryValue, _ := strconv.Atoi(parts[4])
+	if len(args) >= 4 {
+		option := strings.ToUpper(args[2])
+		expiryValue, _ := strconv.Atoi(args[3])
 
 		if option == "EX" {
 			expiry = time.Now().Add(time.Duration(expiryValue) * time.Second)
@@ -128,8 +132,8 @@ func handleSet(conn net.Conn, db *store.DB, parts []string) {
 	conn.Write([]byte("+OK\r\n"))
 }
 
-func handleXAdd(conn net.Conn, db *store.DB, parts []string) {
-	id, err := db.XAdd(parts[1], parts[2], parts[3:])
+func handleXAdd(conn net.Conn, db *store.DB, args []string) {
+	id, err := db.XAdd(args[0], args[1], args[2:])
 	if err != nil {
 		switch err {
 		case store.ErrXAddIDTooSmall:
@@ -144,8 +148,8 @@ func handleXAdd(conn net.Conn, db *store.DB, parts []string) {
 	conn.Write([]byte(fmt.Sprintf("$%d\r\n%s\r\n", len(id), id)))
 }
 
-func handleXRange(conn net.Conn, db *store.DB, parts []string) {
-	entries, err := db.XRange(parts[1], parts[2], parts[3])
+func handleXRange(conn net.Conn, db *store.DB, args []string) {
+	entries, err := db.XRange(args[0], args[1], args[2])
 	if err != nil {
 		conn.Write([]byte("-ERR Invalid stream ID specified as stream command argument\r\n"))
 		return
@@ -165,16 +169,11 @@ func handleXRange(conn net.Conn, db *store.DB, parts []string) {
 	conn.Write([]byte(builder.String()))
 }
 
-func handleIncr(conn net.Conn, db *store.DB, parts []string) {
-	newVal, err := db.Incr(parts[1])
+func handleIncr(conn net.Conn, db *store.DB, args []string) {
+	newVal, err := db.Incr(args[0])
 	if err != nil {
 		conn.Write([]byte("-ERR value is not an integer or out of range\r\n"))
 		return
 	}
 	conn.Write([]byte(fmt.Sprintf(":%d\r\n", newVal)))
-
-}
-
-func handleMulti(conn net.Conn, db *store.DB, parts []string) {
-	conn.Write([]byte("+OK\r\n"))
 }
--- internal/server/handler.go
+++ internal/server/handler.go
@@ -1,10 +1,12 @@
 package server
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"net"
 	"strings"
+	"time"
 
 	"github.com/GARMA-A/redisgo/internal/resp"
 	"github.com/GARMA-A/redisgo/internal/store"
@@ -12,6 +14,9 @@ import (
 
 func HandleClient(conn net.Conn, db *store.DB) {
 	defer conn.Close()
+
+	inMulti := false
+	queuedCommands := make([][]string, 0)
 
 	buf := make([]byte, 1024)
 	for {
@@ -30,205 +35,186 @@ func HandleClient(conn net.Conn, db *store.DB) {
 		if len(parts) == 0 || parts[0] == "" {
 			continue
 		}
+
 		command := strings.ToUpper(parts[0])
-		handleCommand(command, parts[1:], db, conn)
+		args := parts[1:]
+
+		switch command {
+		case "MULTI":
+			if len(args) != 0 {
+				conn.Write([]byte("-ERR wrong number of arguments\r\n"))
+				continue
+			}
+			inMulti = true
+			queuedCommands = queuedCommands[:0]
+			conn.Write([]byte("+OK\r\n"))
+
+		case "EXEC":
+			if len(args) != 0 {
+				conn.Write([]byte("-ERR wrong number of arguments\r\n"))
+				continue
+			}
+			if !inMulti {
+				conn.Write([]byte("-ERR EXEC without MULTI\r\n"))
+				continue
+			}
+
+			if len(queuedCommands) == 0 {
+				inMulti = false
+				queuedCommands = queuedCommands[:0]
+				conn.Write([]byte("*0\r\n"))
+				continue
+			}
+
+			responses := make([][]byte, 0, len(queuedCommands))
+			for _, queued := range queuedCommands {
+				if len(queued) == 0 {
+					responses = append(responses, []byte("-ERR unknown command\r\n"))
+					continue
+				}
+				capture := &bufferConn{}
+				executeCommand(strings.ToUpper(queued[0]), queued[1:], db, capture)
+				responses = append(responses, capture.Bytes())
+			}
+
+			inMulti = false
+			queuedCommands = queuedCommands[:0]
+
+			var out strings.Builder
+			out.WriteString(fmt.Sprintf("*%d\r\n", len(responses)))
+			for _, response := range responses {
+				out.Write(response)
+			}
+			conn.Write([]byte(out.String()))
+
+		default:
+			if inMulti {
+				queuedCopy := append([]string(nil), parts...)
+				queuedCommands = append(queuedCommands, queuedCopy)
+				conn.Write([]byte("+QUEUED\r\n"))
+				continue
+			}
+
+			executeCommand(command, args, db, conn)
+		}
 	}
 }
 
-func handleCommand(command string, args []string, db *store.DB, conn net.Conn) {
-	var weAreInsideMulti = false
-	var multiCommands [][]string
-
+func executeCommand(command string, args []string, db *store.DB, conn net.Conn) {
 	switch command {
 	case "PING":
+		if len(args) != 0 {
+			conn.Write([]byte("-ERR wrong number of arguments\r\n"))
+			return
+		}
 		conn.Write([]byte("+PONG\r\n"))
+
 	case "ECHO":
-		if len(args) < 1 {
+		if len(args) != 1 {
 			conn.Write([]byte("-ERR wrong number of arguments\r\n"))
 			return
 		}
-		if weAreInsideMulti {
-			multiCommands = append(multiCommands, args)
-			conn.Write([]byte("+QUEUED\r\n"))
-			return
-		}
-		conn.Write([]byte(fmt.Sprintf("$%d\r\n%s\r\n", len(args[1]), args[0])))
+		handleEcho(conn, args)
+
 	case "SET":
-		if len(args) < 1 {
+		if len(args) < 2 {
 			conn.Write([]byte("-ERR wrong number of arguments\r\n"))
-			return
-		}
-
-		if weAreInsideMulti {
-			multiCommands = append(multiCommands, args)
-
-			conn.Write([]byte("+QUEUED\r\n"))
 			return
 		}
 		handleSet(conn, db, args)
+
 	case "GET":
-		if len(args) < 1 {
+		if len(args) != 1 {
 			conn.Write([]byte("-ERR wrong number of arguments\r\n"))
 			return
 		}
-
-		if weAreInsideMulti {
-			multiCommands = append(multiCommands, args)
-
-			conn.Write([]byte("+QUEUED\r\n"))
-			return
-		}
 		handleGet(conn, db, args)
+
 	case "RPUSH":
 		if len(args) < 2 {
 			conn.Write([]byte("-ERR wrong number of arguments\r\n"))
 			return
 		}
-
-		if weAreInsideMulti {
-			multiCommands = append(multiCommands, args)
-
-			conn.Write([]byte("+QUEUED\r\n"))
-			return
-		}
 		handleRPush(conn, db, args)
+
 	case "LRANGE":
-		if len(args) < 3 {
+		if len(args) != 3 {
 			conn.Write([]byte("-ERR wrong number of arguments\r\n"))
 			return
 		}
-
-		if weAreInsideMulti {
-			multiCommands = append(multiCommands, args)
-
-			conn.Write([]byte("+QUEUED\r\n"))
-			return
-		}
 		handleLRange(conn, db, args)
+
 	case "LPUSH":
 		if len(args) < 2 {
 			conn.Write([]byte("-ERR wrong number of arguments\r\n"))
 			return
 		}
-
-		if weAreInsideMulti {
-			multiCommands = append(multiCommands, args)
-
-			conn.Write([]byte("+QUEUED\r\n"))
-			return
-		}
 		handleLPush(conn, db, args)
+
 	case "LLEN":
-		if len(args) < 1 {
+		if len(args) != 1 {
 			conn.Write([]byte("-ERR wrong number of arguments\r\n"))
-			return
-		}
-
-		if weAreInsideMulti {
-			multiCommands = append(multiCommands, args)
-
-			conn.Write([]byte("+QUEUED\r\n"))
 			return
 		}
 		handleLLEN(conn, db, args)
+
 	case "LPOP":
-		if len(args) < 1 {
+		if len(args) < 1 || len(args) > 2 {
 			conn.Write([]byte("-ERR wrong number of arguments\r\n"))
 			return
 		}
-
-		if weAreInsideMulti {
-			multiCommands = append(multiCommands, args)
-
-			conn.Write([]byte("+QUEUED\r\n"))
-			return
-		}
 		handleLPop(conn, db, args)
+
 	case "BLPOP":
 		if len(args) < 2 {
 			conn.Write([]byte("-ERR wrong number of arguments\r\n"))
 			return
 		}
-
-		if weAreInsideMulti {
-			multiCommands = append(multiCommands, args)
-
-			conn.Write([]byte("+QUEUED\r\n"))
-			return
-		}
 		handleBLPOP(conn, db, args)
+
 	case "XADD":
-		if len(args) < 4 {
+		if len(args) < 4 || (len(args)-2)%2 != 0 {
 			conn.Write([]byte("-ERR wrong number of arguments\r\n"))
 			return
 		}
-
-		if weAreInsideMulti {
-			multiCommands = append(multiCommands, args)
-
-			conn.Write([]byte("+QUEUED\r\n"))
-			return
-		}
 		handleXAdd(conn, db, args)
+
 	case "XRANGE":
 		if len(args) != 3 {
 			conn.Write([]byte("-ERR wrong number of arguments\r\n"))
 			return
 		}
-
-		if weAreInsideMulti {
-			multiCommands = append(multiCommands, args)
-
-			conn.Write([]byte("+QUEUED\r\n"))
-			return
-		}
 		handleXRange(conn, db, args)
 
 	case "TYPE":
-		if len(args) < 1 {
+		if len(args) != 1 {
 			conn.Write([]byte("-ERR wrong number of arguments\r\n"))
-			return
-		}
-
-		if weAreInsideMulti {
-			multiCommands = append(multiCommands, args)
-
-			conn.Write([]byte("+QUEUED\r\n"))
 			return
 		}
 		handleType(conn, db, args)
 
 	case "INCR":
-		if len(args) < 1 {
+		if len(args) != 1 {
 			conn.Write([]byte("-ERR wrong number of arguments\r\n"))
-			return
-		}
-
-		if weAreInsideMulti {
-			multiCommands = append(multiCommands, args)
-			conn.Write([]byte("+QUEUED\r\n"))
 			return
 		}
 		handleIncr(conn, db, args)
 
-	case "MULTI":
-		if len(args) != 0 {
-			conn.Write([]byte("-ERR wrong number of arguments\r\n"))
-			return
-		}
-		weAreInsideMulti = true
-		conn.Write([]byte("+OK\r\n"))
-	case "EXEC":
-		if !weAreInsideMulti || len(args) != 0 {
-			conn.Write([]byte("-ERR EXEC without MULTI\r\n"))
-		}
-		if weAreInsideMulti && len(multiCommands) == 0 {
-			weAreInsideMulti = false
-			conn.Write([]byte("*0\r\n"))
-		}
-		for _, cmd := range multiCommands {
-			strings.ToUpper(cmd[0])
-			handleCommand(cmd[0], cmd[1:], db, conn)
-		}
+	default:
+		conn.Write([]byte("-ERR unknown command\r\n"))
 	}
 }
+
+type bufferConn struct {
+	buf bytes.Buffer
+}
+
+func (c *bufferConn) Read(_ []byte) (int, error)         { return 0, io.EOF }
+func (c *bufferConn) Write(p []byte) (int, error)        { return c.buf.Write(p) }
+func (c *bufferConn) Close() error                       { return nil }
+func (c *bufferConn) LocalAddr() net.Addr                { return nil }
+func (c *bufferConn) RemoteAddr() net.Addr               { return nil }
+func (c *bufferConn) SetDeadline(_ time.Time) error      { return nil }
+func (c *bufferConn) SetReadDeadline(_ time.Time) error  { return nil }
+func (c *bufferConn) SetWriteDeadline(_ time.Time) error { return nil }
+func (c *bufferConn) Bytes() []byte                      { return c.buf.Bytes() }
