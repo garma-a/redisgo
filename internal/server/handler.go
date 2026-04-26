7798aeca1ef95b34f7a13f13bf9d735f2af66063
Girgis <girgisemad74@gmail.com>
2026-04-26 03:49:45 +0300

    codecrafters submit [skip ci]

--- internal/server/handler.go
+++ internal/server/handler.go
@@ -31,11 +31,11 @@ func HandleClient(conn net.Conn, db *store.DB) {
 			continue
 		}
 		command := strings.ToUpper(parts[0])
-		handleCommand(command, parts[1:], db, conn, parts)
+		handleCommand(command, parts[1:], db, conn)
 	}
 }
 
-func handleCommand(command string, args []string, db *store.DB, conn net.Conn, parts []string) {
+func handleCommand(command string, args []string, db *store.DB, conn net.Conn) {
 	var weAreInsideMulti = false
 	var multiCommands [][]string
 
@@ -43,183 +43,183 @@ func handleCommand(command string, args []string, db *store.DB, conn net.Conn, p
 	case "PING":
 		conn.Write([]byte("+PONG\r\n"))
 	case "ECHO":
-		if len(parts) < 1 {
+		if len(args) < 1 {
 			conn.Write([]byte("-ERR wrong number of arguments\r\n"))
 			return
 		}
 		if weAreInsideMulti {
-			multiCommands = append(multiCommands, parts)
+			multiCommands = append(multiCommands, args)
 			conn.Write([]byte("+QUEUED\r\n"))
 			return
 		}
-		conn.Write([]byte(fmt.Sprintf("$%d\r\n%s\r\n", len(parts[1]), parts[1])))
+		conn.Write([]byte(fmt.Sprintf("$%d\r\n%s\r\n", len(args[1]), parts[1])))
 	case "SET":
-		if len(parts) < 1 {
+		if len(args) < 1 {
 			conn.Write([]byte("-ERR wrong number of arguments\r\n"))
 			return
 		}
 
 		if weAreInsideMulti {
-			multiCommands = append(multiCommands, parts)
+			multiCommands = append(multiCommands, args)
 
 			conn.Write([]byte("+QUEUED\r\n"))
 			return
 		}
-		handleSet(conn, db, parts)
+		handleSet(conn, db, args)
 	case "GET":
-		if len(parts) < 1 {
+		if len(args) < 1 {
 			conn.Write([]byte("-ERR wrong number of arguments\r\n"))
 			return
 		}
 
 		if weAreInsideMulti {
-			multiCommands = append(multiCommands, parts)
+			multiCommands = append(multiCommands, args)
 
 			conn.Write([]byte("+QUEUED\r\n"))
 			return
 		}
-		handleGet(conn, db, parts)
+		handleGet(conn, db, args)
 	case "RPUSH":
-		if len(parts) < 2 {
+		if len(args) < 2 {
 			conn.Write([]byte("-ERR wrong number of arguments\r\n"))
 			return
 		}
 
 		if weAreInsideMulti {
-			multiCommands = append(multiCommands, parts)
+			multiCommands = append(multiCommands, args)
 
 			conn.Write([]byte("+QUEUED\r\n"))
 			return
 		}
-		handleRPush(conn, db, parts)
+		handleRPush(conn, db, args)
 	case "LRANGE":
-		if len(parts) < 3 {
+		if len(args) < 3 {
 			conn.Write([]byte("-ERR wrong number of arguments\r\n"))
 			return
 		}
 
 		if weAreInsideMulti {
-			multiCommands = append(multiCommands, parts)
+			multiCommands = append(multiCommands, args)
 
 			conn.Write([]byte("+QUEUED\r\n"))
 			return
 		}
-		handleLRange(conn, db, parts)
+		handleLRange(conn, db, args)
 	case "LPUSH":
-		if len(parts) < 2 {
+		if len(args) < 2 {
 			conn.Write([]byte("-ERR wrong number of arguments\r\n"))
 			return
 		}
 
 		if weAreInsideMulti {
-			multiCommands = append(multiCommands, parts)
+			multiCommands = append(multiCommands, args)
 
 			conn.Write([]byte("+QUEUED\r\n"))
 			return
 		}
-		handleLPush(conn, db, parts)
+		handleLPush(conn, db, args)
 	case "LLEN":
-		if len(parts) < 1 {
+		if len(args) < 1 {
 			conn.Write([]byte("-ERR wrong number of arguments\r\n"))
 			return
 		}
 
 		if weAreInsideMulti {
-			multiCommands = append(multiCommands, parts)
+			multiCommands = append(multiCommands, args)
 
 			conn.Write([]byte("+QUEUED\r\n"))
 			return
 		}
-		handleLLEN(conn, db, parts)
+		handleLLEN(conn, db, args)
 	case "LPOP":
-		if len(parts) < 1 {
+		if len(args) < 1 {
 			conn.Write([]byte("-ERR wrong number of arguments\r\n"))
 			return
 		}
 
 		if weAreInsideMulti {
-			multiCommands = append(multiCommands, parts)
+			multiCommands = append(multiCommands, args)
 
 			conn.Write([]byte("+QUEUED\r\n"))
 			return
 		}
-		handleLPop(conn, db, parts)
+		handleLPop(conn, db, args)
 	case "BLPOP":
-		if len(parts) < 2 {
+		if len(args) < 2 {
 			conn.Write([]byte("-ERR wrong number of arguments\r\n"))
 			return
 		}
 
 		if weAreInsideMulti {
-			multiCommands = append(multiCommands, parts)
+			multiCommands = append(multiCommands, args)
 
 			conn.Write([]byte("+QUEUED\r\n"))
 			return
 		}
-		handleBLPOP(conn, db, parts)
+		handleBLPOP(conn, db, args)
 	case "XADD":
-		if len(parts) < 4 {
+		if len(args) < 4 {
 			conn.Write([]byte("-ERR wrong number of arguments\r\n"))
 			return
 		}
 
 		if weAreInsideMulti {
-			multiCommands = append(multiCommands, parts)
+			multiCommands = append(multiCommands, args)
 
 			conn.Write([]byte("+QUEUED\r\n"))
 			return
 		}
-		handleXAdd(conn, db, parts)
+		handleXAdd(conn, db, args)
 	case "XRANGE":
-		if len(parts) != 3 {
+		if len(args) != 3 {
 			conn.Write([]byte("-ERR wrong number of arguments\r\n"))
 			return
 		}
 
 		if weAreInsideMulti {
-			multiCommands = append(multiCommands, parts)
+			multiCommands = append(multiCommands, args)
 
 			conn.Write([]byte("+QUEUED\r\n"))
 			return
 		}
-		handleXRange(conn, db, parts)
+		handleXRange(conn, db, args)
 
 	case "TYPE":
-		if len(parts) < 1 {
+		if len(args) < 1 {
 			conn.Write([]byte("-ERR wrong number of arguments\r\n"))
 			return
 		}
 
 		if weAreInsideMulti {
-			multiCommands = append(multiCommands, parts)
+			multiCommands = append(multiCommands, args)
 
 			conn.Write([]byte("+QUEUED\r\n"))
 			return
 		}
-		handleType(conn, db, parts)
+		handleType(conn, db, args)
 
 	case "INCR":
-		if len(parts) < 1 {
+		if len(args) < 1 {
 			conn.Write([]byte("-ERR wrong number of arguments\r\n"))
 			return
 		}
 
 		if weAreInsideMulti {
-			multiCommands = append(multiCommands, parts)
+			multiCommands = append(multiCommands, args)
 			conn.Write([]byte("+QUEUED\r\n"))
 			return
 		}
-		handleIncr(conn, db, parts)
+		handleIncr(conn, db, args)
 
 	case "MULTI":
-		if len(parts) != 0 {
+		if len(args) != 0 {
 			conn.Write([]byte("-ERR wrong number of arguments\r\n"))
 			return
 		}
 		weAreInsideMulti = true
 		conn.Write([]byte("+OK\r\n"))
 	case "EXEC":
-		if !weAreInsideMulti || len(parts) != 0 {
+		if !weAreInsideMulti || len(args) != 0 {
 			conn.Write([]byte("-ERR EXEC without MULTI\r\n"))
 		}
 		if weAreInsideMulti && len(multiCommands) == 0 {
@@ -228,7 +228,7 @@ func handleCommand(command string, args []string, db *store.DB, conn net.Conn, p
 		}
 		for _, cmd := range multiCommands {
 			strings.ToUpper(cmd[0])
-			handleCommand(cmd[0], cmd[1:], db, conn, cmd)
+			handleCommand(cmd[0], cmd[1:], db, conn)
 		}
 	}
 }
