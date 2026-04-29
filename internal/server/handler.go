386e1b7f0625ee5f21e1df3c362fdb95b9838e2a
Girgis <girgisemad74@gmail.com>
2026-04-29 04:17:22 +0300

    codecrafters submit [skip ci]

--- internal/server/handler.go
+++ internal/server/handler.go
@@ -294,9 +294,8 @@ func executeCommand(command string, args []string, db *store.DB, conn net.Conn, 
 			conn.Write([]byte("-ERR wrong number of arguments\r\n"))
 			return
 		}
-		if len(args) > 2 {
+		if args[0] == "GETACK" && args[1] == "*" {
 			conn.Write([]byte("*3\r\n$8\r\nREPLCONF\r\n$3\r\nACK\r\n$1\r\n0\r\n"))
-			return
 		}
 		conn.Write([]byte("+OK\r\n"))
 	case "PSYNC":
